8031424d7d492d47fc97f296fc7e186fb7a09c14
«irinabassina» <albas01@bk.ru>
2023-06-19 00:15:18 +0600

    task_03 is done

---
+++ Task_03/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void SqurePrinter(int numberN)
+{
+    int current = 1;
+    while (current <= numberN)
+    {
+        double res = Math.Pow(current, 3);
+        Console.Write($"{res:f0}, ");
+        current++;
+    }
+    Console.WriteLine("\b\b ");     // удаляет пробел и запятую в конце строки
+}
+void SqurePrinterNeg(int numberN)
+{
+    int current = 1;
+    while (current >= numberN)
+    {
+        double res = Math.Pow(current, 3);
+        Console.Write($"{res:f0}, ");
+        current--;
+    }
+    Console.WriteLine("\b\b ");
+}
+
+Console.Write("Input number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+if (user_number > 0)
+    SqurePrinter(user_number);
+else
+    SqurePrinterNeg(user_number);
+
